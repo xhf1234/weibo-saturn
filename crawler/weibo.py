a39e270c93658f17c838050b9b0a2debb731e04d
xuhf <xuhf@fenbi.com>
2012-10-17 23:59:27 +0800

    ADD: ApiException

--- crawler/weibo.py
+++ crawler/weibo.py
@@ -11,9 +11,19 @@ class WeiboClient(object):
         conn = httplib.HTTPSConnection("api.weibo.com")
         conn.request("GET", "/2/friendships/friends/bilateral.json?uid=%d&access_token=%s" %(uid, access_token))
         resp = conn.getresponse()
-        print resp.status, resp.reason
+        if resp.status != 200:
+            raise ApiException(resp.status, resp.reason)
         strJson = resp.read()
         return User.decodeList(strJson) 
+
+class ApiException(Exception):
+    def __init__(self, status, reason):
+        self.status = status
+        self.reason = reason
+
+    def __str__(self):
+        return "%d %s" %(self.status, self.reason)
+    
 
 if __name__ == '__main__':
     client = WeiboClient()
